d11e16fd86be20496f3bd8f513b7310c45a5e389
Amarok-w <Vadimon4k00073@gmail.com>
2024-12-05 18:55:19 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./openapi.json');
+const path = require('path')
+const {v4} = require('uuid')
+const app = express()
+
+
+const cors = require('cors');
+app.use(cors());
+
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(4000, () => {
+  console.log('Swagger UI запущен на http://localhost:4000/api-docs');
+});
+
+let CONTACTS = [
+  {id: v4(), name: 'Vadim', value: '+7-921-100-20-30', marked: false}
+]
+
+app.use(express.json());
+
+// GET
+app.get('/api/contacts', (req, res) => {
+  setTimeout(() => {
+    res.status(200).json(CONTACTS)
+  }, 1000)
+})
+
+// POST
+app.post('/api/contacts', (req, res) => {
+  const contact = {...req.body, id: v4(), marked: false}
+  CONTACTS.push(contact)
+  res.status(201).json(contact)
+})
+
+// DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+  CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+  res.status(200).json({message: 'Контакт был удален'})
+})
+
+// PUT
+app.put('/api/contacts/:id', (req, res) => {
+  const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+  CONTACTS[idx] = req.body
+  res.json(CONTACTS[idx])
+})
+
+
+app.use(express.static(path.resolve(__dirname, 'client')))
+// app.use(path.resolve(__dirname, 'client'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+})
+
+app.listen(3000, () => console.log('Server has been started on port 3000...'))
